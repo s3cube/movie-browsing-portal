2d627c96ac085631af5125efc1b78f5e9cdb8ef0
Satej Sawant <satejs93@gmail.com>
2019-04-18 19:15:34 -0400

    Added CSS Loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import '../src/css/style.css';
 
 export default class HelloWorld extends Component{
     render(){
         return(
             <div>
+                <h1>HELLO</h1>
                 Hello Tantrum!
             </div>
         )
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ module.exports = {
                         loader:"html-loader"
                     }
                 ]
+            },{
+                test:/\.css$/,
+                use:["style-loader","css-loader"]
             }
         ]
     },

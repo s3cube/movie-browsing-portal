0cec3d173f42367ad466fd51f79eb8dd8b7a0c30
Satej Sawant <satejs93@gmail.com>
2019-04-18 21:28:28 -0400

    Added temporary CSS framework

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import '../src/css/style.css';
+import '../src/css/simple-grid.css';
 
 export default class HelloWorld extends Component{
     render(){
         return(
-            <div>
+            <div className="container">
+                <div className="row-3">
+                </div>
+                <div className="row-3">
                 <h1>HELLO</h1>
+                </div>
+                <div className="row-3">
+                </div>
+               
                 Hello Tantrum!
             </div>
         )

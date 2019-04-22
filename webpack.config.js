1ef85c268205edfe962bc2c16f2ed48678fdd397
Satej Sawant <satejs93@gmail.com>
2019-04-21 23:04:08 -0400

    Changed filename in HtmlWebPackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     plugins:[
         new HtmlWebPackPlugin({
             template:"./src/index.html",
-            filename:"./index.html"
+            filename:"index.html"
         })
     ]
 }
